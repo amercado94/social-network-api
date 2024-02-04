662eb484ce4af0301d68b32cdf0f0bf74e0214bb
Alfredo <alfredomercado1994@gmail.com>
2024-02-04 12:55:19 -0600

    initial test

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Thought',
+          }
+        ],
+        },
+        {
+            toJSON: {
+              virtuals: true, 
+            },
+            id: false, 
+        }
+        );
+
+ userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model ('User', userSchema);
+
+module.exports = User;
